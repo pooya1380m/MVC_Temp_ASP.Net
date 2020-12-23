eb64f7856c600ef5215ee0bbcbf19c6072ea8c20
pooya1380m <58190154+pooya1380m@users.noreply.github.com>
2020-12-23 01:12:33 -0800

    first commit

---
+++ mvc Test/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc_Test.Models;
+using mvc_Test.ViewModels;
+
+namespace mvc_Test.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+           {
+               new Customer {Name = "costumer 1" },
+               new Customer {Name = "costumer 2" }
+               
+           };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Custumers = customers
+
+            };
+            return View(viewModel);
+        }
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year + "//" + month+"hi my name is pooya");
+            
+        }
+
+    } 
+}
---
+++ mvc Test/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using mvc_Test.Models;
+
+namespace mvc_Test.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Custumers { get; set; }
+    }
+}
